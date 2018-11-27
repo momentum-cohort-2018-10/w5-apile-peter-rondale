a189bc074ab27d2b5f890f8eac0c781d3a081189
Peter Oas <oas.peter@gmail.com>
2018-11-27 09:33:02 -0500

    working admin section

--- apile/apile_app/admin.py
+++ apile/apile_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from apile_app.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = ('author', 'title', 'description', 'date_added',)
+    prepopulated_fields = {'slug': ('slug',)}
 
 # Register your models here.
+admin.site.register(Post, PostAdmin)
--- apile/apile_app/models.py
+++ apile/apile_app/models.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 from slugify import slugify
 
+
 # Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey(to='User', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=255)
     description = models.TextField(null=True)
     date_added = models.DateTimeField(auto_now=True)
